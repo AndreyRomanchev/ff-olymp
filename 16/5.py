942129c69f7cac399cd77b31382f6f91a6bcaae9
Andrey V. Romanchev <andrey.romanchev@gmail.com>
2020-02-22 23:49:08 +0300

    lection 16. fix task 5 to run under 1 sec

--- 16/5.py
+++ 16/5.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
-def sieve_fast(n: int) -> list:
+from time import time
+
+def sieve_fast(n: int):
     is_prime = [0] * (n + 1)
+    ans = []
     for d in range(2, n + 1):
         if is_prime[d] == 0:
             for i in range(d, n + 1, d):
                 is_prime[i] += 1
         if is_prime[d] >= 3:
-            print(d, end=' ')
-
+            ans.append(str(d))
+    return ans
 
 n = int(input())
 
-sieve_fast(n)
+print(' '.join(sieve_fast(n)))
